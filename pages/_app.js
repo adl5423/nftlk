48de1c29a5634f446de06af2ee24a954aacea91d
Aisen Lopez <aisenlopez@hotmail.com>
2022-09-19 11:50:16 -0400

    main commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import {
+  chain,
+  WagmiConfig,
+  createClient,
+  configureChains,
+  defaultChains,
+} from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { InjectedConnector } from '@wagmi/core'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.polygonMumbai],
+  [publicProvider()],
+)
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+  connectors: [
+    new CoinbaseWalletConnector({chains}), 
+    new MetaMaskConnector({chains}), 
+    new InjectedConnector({chains, options: {name: "Injected"} })
+  ],
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} /> 
+    </WagmiConfig>
+  );
 }
 
 export default MyApp
